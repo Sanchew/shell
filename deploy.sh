42b3c94e1a8801c721a4023da05b3dd3b01f028d
lee <lijianwei@chicv.com>
2017-03-30 14:54:49 +0800

    u

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 echo 'deploy shell running'
-filename=`ls tmpop -t | head -n 1`
-meta=${filename:8:9}
+filename=`ls output -t | head -n 1`
+meta=`echo $filename | awk '{sub(/^[^.]*./,"");sub(".ipa","");print}'`
 version=${meta%.*}
 env=$(echo ${meta##*.}|tr '[a-z]' '[A-Z]')
+AppName=${filename%.*}
+appname=$(echo $AppName|tr '[A-Z]' '[a-z]')
 
-sed -i "/Install App (/a\\\t\t<div><a class='btn btn-default btn-lg btn-block' style='margin-right:10px' href='itms-services://?action=download-manifest&url=https://github.com/lijianwei-jj/FuckApp/raw/master/manifest.$meta.plist'>Install App $version $env</a></div>" /home/website/views/index.html
+sed -i "/id='tag'/a\\\t\t<div><a class='btn btn-default btn-lg btn-block' style='margin-right:10px' href='itms-services://?action=download-manifest&url=https://github.com/lijianwei-jj/FuckApp/raw/master/manifest.$appname.$meta.plist'>Install $AppName $version $env</a></div>" /home/website/views/index.html
